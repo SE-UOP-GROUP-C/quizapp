8d1369a2161b5682a67af693f77a22addc0de989
Jono <30672425+cutted@users.noreply.github.com>
2020-02-28 19:57:43 +0000

    Update V01.sql

---
+++ fact/assets/database/V01.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE main;
+
+\c main;
+
+CREATE TABLE user_account (
+        reg_uuid serial PRIMARY KEY NOT NULL,
+        reg_email varchar(80) UNIQUE,
+        reg_pass varchar(10),
+        reg_username varchar(40),
+        reg_admin boolean NOT NULL DEFAULT false,
+        reg_social boolean DEFAULT false,
+        reg_noti_email boolean DEFAULT true,
+        reg_noti_push boolean DEFAULT true,
+        reg_noti_time integer DEFAULT (3600),
+        quiz_taken text,
+        fact_taken text
+);
