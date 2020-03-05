de21d6dc481bd17d8720be8b222f9ae0b93754b7
Wauchope <34446437+Wauchope@users.noreply.github.com>
2020-03-05 15:08:49 +0000

    google auth test #2
    last test was a success - user can sign in with google. state not stored.
    
    testing if information about signed in user can be retrieved after sign in.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,10 @@
-function loginAttacher() {
-  window.loginButtonID.addEventListener("click", onLogin)
+function onLogin(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  window.login.content="ID: " + profile.getID() + "\n"
+  + "Full Name: " + profile.getName() + "\n"
+  + "Given Name: " + profile.getGivenName() + "\n"
+  + "Family Name: " + profile.getFamilyName() + "\n"
+  + "Image URL: " + profile.getImageUrl() + "\n"
+  + "Email: " + profile.getEmail(); + "\n"
+
 }
