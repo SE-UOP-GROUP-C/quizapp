46e1341f2db278da55d76586b6310920eb01b762
Wauchope <34446437+Wauchope@users.noreply.github.com>
2020-03-05 15:58:57 +0000

    login script moved
    to external javascript file

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,9 @@
 function onLogin(googleUser) {
   let profile = googleUser.getBasicProfile();
-  window.login.content="ID: " + profile.getID() + "\n"
-  + "Full Name: " + profile.getName() + "\n"
-  + "Given Name: " + profile.getGivenName() + "\n"
-  + "Family Name: " + profile.getFamilyName() + "\n"
-  + "Image URL: " + profile.getImageUrl() + "\n"
-  + "Email: " + profile.getEmail(); + "\n"
-
+  window.login.textContent = "ID: " + profile.getId() + "<br>"
+  + "Full Name: " + profile.getName() + "<br>"
+  + "Given Name: " + profile.getGivenName() + "<br>"
+  + "Family Name: " + profile.getFamilyName() + "<br>"
+  + "Image URL: " + profile.getImageUrl() + "<br>"
+  + "Email: " + profile.getEmail(); + "<br>")
 }
