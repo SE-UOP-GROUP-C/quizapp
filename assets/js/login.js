3a4b57d41cda54742f0a9347223c22135b92b0c3
= <jordan.wauchope@gmail.com>
2020-03-27 07:00:07 +0000

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -19,6 +19,6 @@ function onLogin(googleUser) {
   let jsonToSend = JSON.stringify(dataToSend);
   let request = new XMLHttpRequest();
   request.open('GET', 'https://quizapp-se-uop-6c.herokuapp.com/login', true);
-  request.setRequestHeader("Access-Control-Allow-Origin", "https://se-uop-group-c.github.io/")
+  //request.setRequestHeader("Access-Control-Allow-Origin", "https://se-uop-group-c.github.io/")
   request.send(dataToSend);
 }
