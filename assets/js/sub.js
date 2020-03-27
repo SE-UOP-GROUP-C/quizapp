105812aaf403ccb0ee3246cabba5b68f256d5689
OnIyLegend <50180200+OnIyLegend@users.noreply.github.com>
2020-03-27 00:09:45 +0000

    Final fact submission

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,32 +1,42 @@
-
+/**
+this function was created to validate the inputs from the user
+*/
 function validate(){
-  /* declaring the variables*/
+  /* declaring the variables using id's */
   var topic = document.getElementById("topic").value;
   var fact = document.getElementById("fact").value;
   var email = document.getElementById("email").value;
   var status = document.getElementById("status");
 
-/* conditions to insure the information is correct*/
-  if(email.indexOf("@") == -1 || email.length < 5){
+/*
+Conditions for email making sure that an @ and a . must be included and also having more that 5 characters to insure the email is correct, if the email does not meet one of the criteria a text message below the input boxes will appear informing the user about the email being invalid, so the user can rewrite a correct email.
+*/
+  if(email.indexOf("@") == -1 || email.length < 5 || email.indexOf(".") == -1){
     text = "Please enter valid email";
     status.innerHTML = text;
     return false;
   }
-
-  if(topic.length <= 3){
+/*
+ Having topic length to 3 to insure that the topic is a word with more than 3 words, and an error message indicating if that clause is not met write more than 3 words.
+*/
+  if(topic.length <= 2){
     text = "Please enter a valid topic, more than 3 characters ";
     status.innerHTML = text;
     return false;
   }
-
-  if(fact.length <= 20){
-    text = "Please enter more than 20 characters";
+/*
+ Having fact being more than 10 characters to give enough information to make it a sentence that is about a fact with relevent enough information that the admin can validate and determine that the fact is valid.
+*/
+  if(fact.length <= 10){
+    text = "Please enter more than 10 characters";
     status.innerHTML = text;
     return false;
   }
 
-
-    alert("Your fact was submitted for review");
+/*
+Alerting the user with a pop up message informing them that they need to click on send and their fact will be reviewed, then they can reset the form and send another fact if they wish.
+*/
+    alert("Press send and your fact will be submitted for review");
     return true;
 
   }
@@ -34,10 +44,8 @@ function validate(){
 
 
 /*
-it suppose to be that all 3 valuation need to
- be true so it goes through
 
-sources i used
+personal sources i used to help me with coding:
 https://flaviocopes.com/how-to-string-contains-substring-javascript/
 https://www.w3schools.com/jsref/dom_obj_var.asp
 https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
